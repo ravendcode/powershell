a28b8cb38857e132852e1e0a082cb8e0bfb64da0
ravendcode <ravendcode@gmail.com>
2018-03-05 19:47:57 +0200

    Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,30 @@
 Set-Location D:
+
 # code
 function Js-Code { set-location "D:\JavaScript\Code" }
 Set-Alias jscode Js-Code
+
 function Jsn-Code { set-location "D:\JavaScript\Code\node" }
 Set-Alias jsncode Jsn-Code
+
 function Go-Code { set-location "D:\Go\Code\src\github.com\ravendcode" }
 Set-Alias gocode Go-Code
+
 function Py-Code { set-location "D:\Py\Code" }
 Set-Alias pycode Py-Code
-function Pyb-Code { set-location "D:\Py\Code\base" }
-Set-Alias pybcode Pyb-Code
-function Pys-Code { set-location "D:\Py\Code\sanic" }
-Set-Alias pyscode Pys-Code
-function Pyd-Code { set-location "D:\Py\Code\django" }
-Set-Alias pydcode Pyd-Code
-function Pyf-Code { set-location "D:\Py\Code\flask" }
-Set-Alias pyfcode Pyf-Code
+
+function Py-Base-Code { set-location "D:\Py\Code\base" }
+Set-Alias pybcode Py-Base-Code
+
+function Py-Sanic-Code { set-location "D:\Py\Code\sanic" }
+Set-Alias pyscode Py-Sanic-Code
+
+function Py-Django-Code { set-location "D:\Py\Code\django" }
+Set-Alias pydcode Py-Django-Code
+
+function Py-Flask-Code { set-location "D:\Py\Code\flask" }
+Set-Alias pyfcode Py-Flask-Code
+
 # py venv
 function Py-Venv($venvname) {
     if ($venvname -eq $null) {
@@ -25,37 +34,59 @@ function Py-Venv($venvname) {
     }
 }
 Set-Alias pyv Py-Venv
+
 function Py-Venv-Activate { venv\Scripts\activate }
 Set-Alias pyva Py-Venv-Activate
+
 function Py-Venv-Deactivate { deactivate }
 Set-Alias pyvd Py-Venv-Deactivate
+
+function Pip-Freeze-Requirements { pip freeze > requirements.txt }
+Set-Alias pipf Pip-Freeze-Requirements
+
+function Pip-Install-Requirements { pip install -r requirements.txt }
+Set-Alias pipi Pip-Install-Requirements
+
 # git
 function Git-Init { git init }
 Set-Alias ggi Git-Init
+
 function Git-Status { git status }
 Set-Alias ggs Git-Status
+
 function Git-Add { git add . }
 Set-Alias gga Git-Add
+
 function Git-Commit($message) { git commit -m "$message" }
 Set-Alias ggc Git-Commit
+
 function Git-Log { git log }
 Set-Alias gglog Git-Log
+
 function Git-Log-Oneline { git log --oneline }
 Set-Alias ggl Git-Log-Oneline
+
 function Git-Log-Oneline-Graph { git log --oneline --graph }
 Set-Alias gglg Git-Log-Oneline-Graph
+
 function Git-B() { git branch }
 Set-Alias ggb Git-B
+
 function Git-Co($branchname) { git co $branchname}
 Set-Alias ggco Git-Co
+
 function Git-Co-Master { git co master }
 Set-Alias ggcom Git-Co-Master
+
 function Git-Co-Branch($branch) { git co -b $branch }
 Set-Alias ggcob Git-Co-Branch
+
 function Git-Push { git push }
 Set-Alias ggp Git-Push
+
 function Git-Pull { git pull }
 Set-Alias ggpl Git-Pull
+
 # file
 function touch() {
     $t = $host.ui.RawUI.ForegroundColor
@@ -73,6 +104,7 @@ function touch() {
     }
     $host.ui.RawUI.ForegroundColor = $t
 }
+
 function mkdirs() {
     foreach ($dirname in $args) {
         if (! (Test-Path $dirname)) {
@@ -80,6 +112,7 @@ function mkdirs() {
         }
    }
 }
+
 function mcd($dirname) {
     if ($dirname) {
         if (! (Test-Path $dirname)) {
@@ -88,10 +121,12 @@ function mcd($dirname) {
         Set-Location $dirname
     }
 }
+
 function la() {
     gci -force | ?{$_.mode -match "h"}
     gci
 }
+
 function rmrf() {
     foreach ($dirname in $args) {
         if (Test-Path $dirname) {
